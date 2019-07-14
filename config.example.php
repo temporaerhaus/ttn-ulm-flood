39db9ea83e986faad6a70379becc782f5d9755a3
Matthias Schneider <ms@flurp.de>
2019-07-14 23:07:36 +0200

    added sensors to config example

--- config.example.php
+++ config.example.php
@@ -6,3 +6,10 @@ $config = [
     'influx_username' => '',
     'influx_password' => '',
 ];
+
+// hard coded sensor list. for the moment much easier
+// than getting this from the influx DB.
+$cfg_sensors = [
+    1 => 'ultrasonic1', // Herdbrücke
+    2 => 'ultrasonic2', // Eisenbahnbrücke Blau
+];
